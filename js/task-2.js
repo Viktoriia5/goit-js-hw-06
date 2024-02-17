5f0f2ce7633ca6fa67b215ae559881e74c417c0a
Viktoriia Shpak <viktoriia.ienko@gmail.com>
2024-02-17 18:04:48 +0200

    added 3 files with homework 6

---
+++ js/task-2.js
@@ -0,0 +1,26 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    let indexToDelete = this.#items.indexOf(itemToRemove);
+    this.#items.splice(indexToDelete, 1);
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
